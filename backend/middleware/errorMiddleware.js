741b289b324af9e6439e18a9397289a369a40923
BlakeVidebeck <blakevidebeck@gmail.com>
2021-01-06 19:42:45 +1300

    create redux store

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,11 +1,13 @@
-// eg. /api/test
+// 404 error (not found) eg. /api/test
 const notFound = (req, res, next) => {
 	const error = new Error(`Not found - ${req.originalUrl}`)
 	res.status(404)
 	next(error)
 }
 
-// eg. api/products/1 || api/products/5ff511f671ac0024e889bf70
+// custom error handler
+// every error that is thrown goes through this function
+// err is first so you can overwrite the default error middleware
 const errorHandler = (err, req, res, next) => {
 	const statusCode = res.statusCode === 200 ? 500 : res.statusCode
 	res.status(statusCode)
