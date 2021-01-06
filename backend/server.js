afb1046a1bf6400ec8b7e93a4d13527891e3c787
BlakeVidebeck <blakevidebeck@gmail.com>
2021-01-06 15:09:45 +1300

    fetch products from db

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
-// connect to the database
 connectDB()
 
 const app = express()
@@ -15,14 +14,7 @@ app.get('/', (req, res) => {
 	res.send('Hello')
 })
 
-app.get('/api/products', (req, res) => {
-	res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find(product => (product._id = req.params.id))
-	res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 
