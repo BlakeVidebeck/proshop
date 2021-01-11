7833329e132e9f66af0b61bd04c265a04643493e
BlakeVidebeck <blakevidebeck@gmail.com>
2021-01-11 14:43:42 +1300

    profile screen and functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,7 @@ import {
 	authUser,
 	registerUser,
 	getUserProfile,
+	updateUserProfile,
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -11,6 +12,9 @@ router.route('/').post(registerUser)
 // routes should just point to controller functions
 router.post('/login', authUser)
 // the protect middleware is going to run when we try hit this route
-router.route('/profile').get(protect, getUserProfile)
+router
+	.route('/profile')
+	.get(protect, getUserProfile)
+	.put(protect, updateUserProfile)
 
 export default router
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import {
+	USER_DETAILS_FAIL,
+	USER_DETAILS_REQUEST,
+	USER_DETAILS_SUCCESS,
 	USER_LOGIN_FAIL,
 	USER_LOGIN_REQUEST,
 	USER_LOGIN_SUCCESS,
@@ -7,8 +10,12 @@ import {
 	USER_REGISTER_FAIL,
 	USER_REGISTER_REQUEST,
 	USER_REGISTER_SUCCESS,
+	USER_UPDATE_PROFILE_FAIL,
+	USER_UPDATE_PROFILE_REQUEST,
+	USER_UPDATE_PROFILE_SUCCESS,
 } from '../constants/userConstants'
 
+// User login
 export const login = (email, password) => async dispatch => {
 	try {
 		dispatch({
@@ -47,11 +54,13 @@ export const login = (email, password) => async dispatch => {
 	}
 }
 
+// User logout
 export const logout = () => dispatch => {
 	localStorage.removeItem('userInfo')
 	dispatch({ type: USER_LOGOUT })
 }
 
+// User register
 export const register = (name, email, password) => async dispatch => {
 	try {
 		dispatch({
@@ -87,6 +96,83 @@ export const register = (name, email, password) => async dispatch => {
 	} catch (error) {
 		dispatch({
 			type: USER_REGISTER_FAIL,
+			payload:
+				// generic message && custom error message ? custom error message : generic message
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		})
+	}
+}
+
+export const getUserDetails = id => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: USER_DETAILS_REQUEST,
+		})
+
+		// destructure the userInfo from the state
+		const {
+			userLogin: { userInfo },
+		} = getState()
+
+		// pass in the token from the user state to access protected routes
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		}
+
+		// make a request to the backend to get the user object & token
+		const { data } = await axios.get(`/api/users/${id}`, config)
+
+		dispatch({
+			type: USER_DETAILS_SUCCESS,
+			payload: data,
+		})
+	} catch (error) {
+		dispatch({
+			type: USER_DETAILS_FAIL,
+			payload:
+				// generic message && custom error message ? custom error message : generic message
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		})
+	}
+}
+
+// update user profile
+export const updateUserProfile = user => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: USER_UPDATE_PROFILE_REQUEST,
+		})
+
+		// destructure the userInfo from the state
+		const {
+			userLogin: { userInfo },
+		} = getState()
+
+		// pass in the token from the user state to access protected routes
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		}
+
+		// make a request to the backend to get the user object & token
+		const { data } = await axios.put(`/api/users/profile`, user, config)
+
+		dispatch({
+			type: USER_UPDATE_PROFILE_SUCCESS,
+			payload: data,
+		})
+	} catch (error) {
+		dispatch({
+			type: USER_UPDATE_PROFILE_FAIL,
 			payload:
 				// generic message && custom error message ? custom error message : generic message
 				error.response && error.response.data.message
