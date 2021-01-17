dabe7b68f94ef2c3e827c5ceb553615c36a88f06
BlakeVidebeck <blakevidebeck@gmail.com>
2021-01-18 12:53:43 +1300

    fix update navbar name bug

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -13,6 +13,7 @@ import {
 	USER_UPDATE_PROFILE_REQUEST,
 	USER_UPDATE_PROFILE_SUCCESS,
 	USER_UPDATE_PROFILE_FAIL,
+	USER_UPDATE_PROFILE_RESET,
 	USER_LIST_REQUEST,
 	USER_LIST_SUCCESS,
 	USER_LIST_FAIL,
@@ -87,6 +88,8 @@ export const userUpdateProfileReducer = (state = {}, action) => {
 			return { loading: false, success: true, userInfo: payload }
 		case USER_UPDATE_PROFILE_FAIL:
 			return { loading: false, error: payload }
+		case USER_UPDATE_PROFILE_RESET:
+			return {}
 		default:
 			return state
 	}
