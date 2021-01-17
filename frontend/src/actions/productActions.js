8c8f4a35f51d6861495e7971637eec3061616c70
BlakeVidebeck <blakevidebeck@gmail.com>
2021-01-17 17:39:34 +1300

    add front end product reviews & add review

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -2,20 +2,23 @@ import axios from 'axios'
 
 import {
 	PRODUCT_LIST_REQUEST,
-	PRODUCT_LIST_SUCCEESS,
+	PRODUCT_LIST_SUCCESS,
 	PRODUCT_LIST_FAIL,
 	PRODUCT_DETAILS_REQUEST,
-	PRODUCT_DETAILS_SUCCEESS,
+	PRODUCT_DETAILS_SUCCESS,
 	PRODUCT_DETAILS_FAIL,
-	PRODUCT_DELETE_SUCCEESS,
+	PRODUCT_DELETE_SUCCESS,
 	PRODUCT_DELETE_FAIL,
 	PRODUCT_DELETE_REQUEST,
 	PRODUCT_CREATE_REQUEST,
-	PRODUCT_CREATE_SUCCEESS,
+	PRODUCT_CREATE_SUCCESS,
 	PRODUCT_CREATE_FAIL,
 	PRODUCT_UPDATE_REQUEST,
-	PRODUCT_UPDATE_SUCCEESS,
+	PRODUCT_UPDATE_SUCCESS,
 	PRODUCT_UPDATE_FAIL,
+	PRODUCT_CREATE_REVIEW_REQUEST,
+	PRODUCT_CREATE_REVIEW_SUCCESS,
+	PRODUCT_CREATE_REVIEW_FAIL,
 } from '../constants/productConstants'
 import { logout } from './userActions'
 
@@ -27,7 +30,7 @@ export const listProducts = () => async (dispatch) => {
 		// request to the backend for the products
 		const { data } = await axios.get('/api/products')
 
-		dispatch({ type: PRODUCT_LIST_SUCCEESS, payload: data })
+		dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data })
 	} catch (error) {
 		dispatch({
 			type: PRODUCT_LIST_FAIL,
@@ -48,7 +51,7 @@ export const listProductDetails = (id) => async (dispatch) => {
 		// request to the backend for the single product
 		const { data } = await axios.get(`/api/products/${id}`)
 
-		dispatch({ type: PRODUCT_DETAILS_SUCCEESS, payload: data })
+		dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data })
 	} catch (error) {
 		dispatch({
 			type: PRODUCT_DETAILS_FAIL,
@@ -84,7 +87,7 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
 		await axios.delete(`/api/products/${id}`, config)
 
 		dispatch({
-			type: PRODUCT_DELETE_SUCCEESS,
+			type: PRODUCT_DELETE_SUCCESS,
 		})
 	} catch (error) {
 		const message =
@@ -124,7 +127,7 @@ export const createProduct = (id) => async (dispatch, getState) => {
 		const { data } = await axios.post(`/api/products`, {}, config)
 
 		dispatch({
-			type: PRODUCT_CREATE_SUCCEESS,
+			type: PRODUCT_CREATE_SUCCESS,
 			payload: data,
 		})
 	} catch (error) {
@@ -170,7 +173,7 @@ export const updateProduct = (product) => async (dispatch, getState) => {
 		)
 
 		dispatch({
-			type: PRODUCT_UPDATE_SUCCEESS,
+			type: PRODUCT_UPDATE_SUCCESS,
 			payload: data,
 		})
 	} catch (error) {
@@ -183,6 +186,50 @@ export const updateProduct = (product) => async (dispatch, getState) => {
 		}
 		dispatch({
 			type: PRODUCT_UPDATE_FAIL,
+			payload: message,
+		})
+	}
+}
+
+// Create a review on a product
+export const createProductReview = (productId, review) => async (
+	dispatch,
+	getState
+) => {
+	try {
+		dispatch({
+			type: PRODUCT_CREATE_REVIEW_REQUEST,
+		})
+
+		// destructure the userInfo from the state
+		const {
+			userLogin: { userInfo },
+		} = getState()
+
+		// pass in the token from the user state to access protected routes
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		}
+
+		// make a request to the backend
+		await axios.post(`/api/products/${productId}/reviews`, review, config)
+
+		dispatch({
+			type: PRODUCT_CREATE_REVIEW_SUCCESS,
+		})
+	} catch (error) {
+		const message =
+			error.response && error.response.data.message
+				? error.response.data.message
+				: error.message
+		if (message === 'Not authorized, token failed') {
+			dispatch(logout())
+		}
+		dispatch({
+			type: PRODUCT_CREATE_REVIEW_FAIL,
 			payload: message,
 		})
 	}
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,21 +1,25 @@
 import {
 	PRODUCT_LIST_REQUEST,
-	PRODUCT_LIST_SUCCEESS,
+	PRODUCT_LIST_SUCCESS,
 	PRODUCT_LIST_FAIL,
 	PRODUCT_DETAILS_REQUEST,
-	PRODUCT_DETAILS_SUCCEESS,
+	PRODUCT_DETAILS_SUCCESS,
 	PRODUCT_DETAILS_FAIL,
 	PRODUCT_DELETE_REQUEST,
-	PRODUCT_DELETE_SUCCEESS,
+	PRODUCT_DELETE_SUCCESS,
 	PRODUCT_DELETE_FAIL,
 	PRODUCT_CREATE_RESET,
 	PRODUCT_CREATE_REQUEST,
-	PRODUCT_CREATE_SUCCEESS,
+	PRODUCT_CREATE_SUCCESS,
 	PRODUCT_CREATE_FAIL,
-	PRODUCT_UPDATE_SUCCEESS,
+	PRODUCT_UPDATE_SUCCESS,
 	PRODUCT_UPDATE_FAIL,
 	PRODUCT_UPDATE_RESET,
 	PRODUCT_UPDATE_REQUEST,
+	PRODUCT_CREATE_REVIEW_REQUEST,
+	PRODUCT_CREATE_REVIEW_SUCCESS,
+	PRODUCT_CREATE_REVIEW_FAIL,
+	PRODUCT_CREATE_REVIEW_RESET,
 } from '../constants/productConstants'
 
 // get all products
@@ -25,7 +29,7 @@ export const productListReducer = (state = { products: [] }, action) => {
 	switch (type) {
 		case PRODUCT_LIST_REQUEST:
 			return { loading: true, products: [] }
-		case PRODUCT_LIST_SUCCEESS:
+		case PRODUCT_LIST_SUCCESS:
 			return { loading: false, products: payload }
 		case PRODUCT_LIST_FAIL:
 			return { loading: false, error: payload }
@@ -44,7 +48,7 @@ export const productDetailsReducer = (
 	switch (type) {
 		case PRODUCT_DETAILS_REQUEST:
 			return { loading: true, ...state }
-		case PRODUCT_DETAILS_SUCCEESS:
+		case PRODUCT_DETAILS_SUCCESS:
 			return { loading: false, product: payload }
 		case PRODUCT_DETAILS_FAIL:
 			return { loading: false, error: payload }
@@ -60,7 +64,7 @@ export const productDeleteReducer = (state = {}, action) => {
 	switch (type) {
 		case PRODUCT_DELETE_REQUEST:
 			return { loading: true, ...state }
-		case PRODUCT_DELETE_SUCCEESS:
+		case PRODUCT_DELETE_SUCCESS:
 			return { loading: false, success: true }
 		case PRODUCT_DELETE_FAIL:
 			return { loading: false, error: payload }
@@ -76,7 +80,7 @@ export const productCreateReducer = (state = {}, action) => {
 	switch (type) {
 		case PRODUCT_CREATE_REQUEST:
 			return { loading: true, ...state }
-		case PRODUCT_CREATE_SUCCEESS:
+		case PRODUCT_CREATE_SUCCESS:
 			return { loading: false, success: true, product: payload }
 		case PRODUCT_CREATE_FAIL:
 			return { loading: false, error: payload }
@@ -94,12 +98,30 @@ export const productUpdateReducer = (state = { product: {} }, action) => {
 	switch (type) {
 		case PRODUCT_UPDATE_REQUEST:
 			return { loading: true, ...state }
-		case PRODUCT_UPDATE_SUCCEESS:
+		case PRODUCT_UPDATE_SUCCESS:
 			return { loading: false, success: true, product: payload }
 		case PRODUCT_UPDATE_FAIL:
 			return { loading: false, error: payload }
 		case PRODUCT_UPDATE_RESET:
 			return { product: {} }
+		default:
+			return state
+	}
+}
+
+// review on a product
+export const productReviewCreateReducer = (state = {}, action) => {
+	const { type, payload } = action
+
+	switch (type) {
+		case PRODUCT_CREATE_REVIEW_REQUEST:
+			return { loading: true, ...state }
+		case PRODUCT_CREATE_REVIEW_SUCCESS:
+			return { loading: false, success: true }
+		case PRODUCT_CREATE_REVIEW_FAIL:
+			return { loading: false, error: payload }
+		case PRODUCT_CREATE_REVIEW_RESET:
+			return {}
 		default:
 			return state
 	}
